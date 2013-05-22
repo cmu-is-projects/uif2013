46c7d62e8d092e1be866b3d60667e610a64a694e
Stephany Park <park.stephany@gmail.com>
2013-05-21 20:21:34 -0400

    testing

--- test/unit/guardian_test.rb
+++ test/unit/guardian_test.rb
@@ -12,8 +12,45 @@ class GuardianTest < ActiveSupport::TestCase
   #Test Values for cellphone
   should allow_value("412-222-2222").for(:cell_phone)
   should allow_value("2121231234").for(:cell_phone)
-  should_not allow_value("four").for(:section_id)
-  should_not allow_value(-4).for(:section_id)
+  should_not allow_value("hi").for(:cell_phone)
+  should_not allow_value("#&").for(:cell_phone)
   #Test Values for email
+  should allow_value("me@example.com").for(:email)
+  should_not allow_value("example.com").for(:email)
+  should_not allow_value("e4567").for(:email)
 
+  context "3 Guardians" do
+    # create the objects I want with factories
+    setup do
+      create_household_context
+      create_guardian_context
+    end
+
+    # and provide a teardown method as well
+    teardown do
+      remove_household_context
+      remove_guardian_context
+    end
+
+    # test the scope 'alphabetical'
+    should "shows 3 guardians in an alphabetical order" do
+      assert_equal ["Scott, Clark", "Shroot, Mary", "Smith, Larry"], Guardian.alphabetical.map{|s| s.name}
+    end
+
+    #name method
+    should "have working name method" do 
+      assert_equal "Smith, Larry", @lsmith.name
+    end
+
+    #proper_name method
+	should "have working proper_name method" do 
+      assert_equal "Larry Smith", @lsmith.proper_name
+    end
+
+    #reformat_phone method
+    should "reformat phone number" do
+      assert_equal "4122222222", @cscott.cell_phone
+    end
+
+  end
 end
