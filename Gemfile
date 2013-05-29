64c6c877397090179bd2a3475443be724083227a
Wan Xin Teo <wanxint@andrew.cmu.edu>
2013-05-29 11:22:42 -0400

    changed gemfile

--- Gemfile
+++ Gemfile
@@ -49,10 +49,10 @@ end
 
 group :test do
 	gem 'sqlite3', '1.3.7'
-	gem 'shoulda', '3.3.2'
-	gem 'shoulda-matchers', '1.4.2'
+	gem 'shoulda', '3.5.0'
+	gem 'shoulda-matchers', '2.1.0'
 	gem 'factory_girl_rails', '4.2.1'
-	gem 'mocha', '0.10.5'
+	gem 'mocha', '0.14.0'
 	gem 'cucumber-rails', '1.3.0', :require => false
 	gem 'gherkin', '2.11.6'
 	gem 'capybara', '2.0.2'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,13 +14,13 @@ class ActiveSupport::TestCase
   # Context for volunteers
   def create_volunteer_contex
     @brian = FactoryGirl.create(:volunteer)
-    #@james = FactoryGirl.create(:volunteer, first_name: "James", last_name: "Peters", barcode_number: "111000999888")
+    @james = FactoryGirl.create(:volunteer, first_name: "James", last_name: "Peters", barcode_number: "111000999888")
     #@dan = FactoryGirl.create(:volunteer, first_name: "Dan", last_name: "Humphrey", barcode_number: "123098123567",app_approved: false)
   end
   
   def remove_volunteer_context
     @brian.delete
-    #@james.delete
+    @james.delete
     #@dan.delete
   end
   
