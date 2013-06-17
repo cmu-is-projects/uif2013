abc324e769d5707aaf883d614e453c88d6239ba2
Vivek Sainanee <sainanee@cmu.edu>
2013-06-17 12:18:24 -0400

    expiring check implemented!

--- app/models/volunteer_check.rb
+++ app/models/volunteer_check.rb
@@ -8,26 +8,13 @@ class VolunteerCheck < ActiveRecord::Base
     validates_presence_of :check_id, :volunteer_id
     
     scope :pending, where('date_completed IS NULL')
-    #   scope :expiring, joins(:check).where('duration > ?', (Date.today.month).to_i)
-    
-    scope :expiring, joins("LEFT JOIN checks ON checks.id = volunteer_checks.check_id").where('duration > ?', 2)
+    scope :completed, where('date_completed IS NOT NULL')
+ 
 
-    
-    #    scope :expiring, joins("LEFT JOIN checks ON checks.id = volunteer_checks.check_id").where('duration > ?', (Date.today - :date_completed).months.to_s.to_i)
+	def self.expiring
+		VolunteerCheck.completed.select{ |vc| (( vc.date_completed + vc.check.duration.months ) - Date.today).to_i < 30}
+	end
 
-    # scope :expiring, joins("LEFT JOIN checks ON checks.id = volunteer_checks.check_id").where('duration > ?', (Date.today - :date_completed).to_s.to_i)
 
-    
-    #    def time_before_expiration
-        # ShiftJob.find_all_by_shift_id(self.id).size > 0
-    #   (Date.today - date_completed).to_i
-    #end
-
-    
-    
-    # scope :expiring, joins(:check).where('duration > ?', Date.today-date_completed)
-    
-    
-    #    scope :by_store, joins(:assignment, :store).order(:name)
 
 end
